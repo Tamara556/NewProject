c0ae7dd288d6a7fc60c091980a2d4217a2145492
Tamara Murazyan <murazyan.tamara5566.com>
2024-07-07 14:10:32 +0300

    first homework

---
+++ src/homework1/ForlfExamples.java
@@ -0,0 +1,15 @@
+package homework1;
+
+public class ForlfExamples {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 11; x = x + 1)
+            System.out.println("x;" + x);
+        int y;
+        for (y = 50; y > 0; y = y - 1)
+            System.out.println("y;" + y);
+        int a;
+        for (a = 1; a < 20; a = a + 2)
+            System.out.println("a;" + a);
+    }
+}
