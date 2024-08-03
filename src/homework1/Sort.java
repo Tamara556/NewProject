4e08e0b7356b6632983643ee0352aebca9fea33f
Tamara Murazyan <murazyan.tamara5566.com>
2024-08-03 16:07:23 +0300

    5rd homework

---
+++ src/homework1/Sort.java
@@ -0,0 +1,23 @@
+package homework1;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] numbers = {1, 5, 6, 7, 2, 0, 3, 4};
+        int n = numbers.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j]> numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
